dc5ff3aa5b37c87584fabf52adb2910590d04cdc
Sazan23 <5domino@mail.ru>
2022-09-03 18:32:33 +0300

    Added a form for uploading a file to the main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,15 @@
 @section('content')
   <div class="p-3 pb-md-4 mx-auto text-center">
     <h1 class="display-4 fw-normal">Главная</h1>
+    <form enctype="multipart/form-data" action="/upload" method="POST">
+      <div class="mb-3">
+        <label for="formFile" class="form-label">Загрузить файл Excel: </label>
+        <input type="hidden" name="MAX_FILE_SIZE" value="50000" />
+        <input class="form-control" type="file" name="xls_file" />
+      </div>
+      <input type="submit" value="Отправить файл" />
+    </form>
+    <hr>
     <p class="fs-5 text-start text-muted">1.  Загрузка excel-файл в базу данных.</p>
     <p class="fs-5 text-start text-muted">2.  Вывод списка загруженных файлов.</p>
     <p class="fs-5 text-start text-muted">3.  Просмотр и редактирование строк с данными разных типов (изображение, текстовое поле, поле для выбора даты) для выбранного файла. Выгрузка записи в виде PDF.</p>
