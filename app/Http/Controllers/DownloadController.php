680d73740f3c9750ee05d37404268d3e799a19f5
Sazan23 <5domino@mail.ru>
2022-09-09 15:04:49 +0300

    Создан класс-обёртка для TCPDF

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Utility\PHPExcel_Utils;
-use App\Models\Files;
-use App\Models\Records;
-use TCPDF;
+use App\Utility\TCPDF_Outer;
 
 class DownloadController extends Controller
 {
@@ -29,73 +27,10 @@ class DownloadController extends Controller
      * @param  \Illuminate\Http\Request $request
      * @return void
      */
-    public function downloadPDF_record(Request $request, $file_id, $id )
+    public function downloadPDF_record(Request $request, $file_id, $id)
     {
-        $file = Files::find($file_id);
-        $record = Records::where('file_id', $file_id)
-                         ->where('id', $id)
-                         ->first();
-        // create new PDF document
-        $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8');
-
-        // set document information
-        $pdf->setCreator(PDF_CREATOR);
-        $pdf->setAuthor('Oleg Denisenko');
-        $pdf->setTitle('TCPDF Example 009');
-        $pdf->setSubject('TCPDF Tutorial');
-        $pdf->setKeywords('TCPDF, PDF, example, test, guide');
-
-        // set default header data
-        $pdf_header_string= "Отчёт по записи №". $id. "\nиз файла " . $file->file_name;
-        $pdf->setHeaderData('logo.jpg', 20, PDF_HEADER_TITLE, $pdf_header_string);
-
-        // set header and footer fonts
-        $pdf->setHeaderFont(['dejavusans', '', PDF_FONT_SIZE_MAIN]);
-        $pdf->setFooterFont(['dejavusans', '', PDF_FONT_SIZE_DATA]);
-
-        // set default monospaced font
-        $pdf->setDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
-        // set margins
-        $pdf->setMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-        $pdf->setHeaderMargin(PDF_MARGIN_HEADER);
-        $pdf->setFooterMargin(PDF_MARGIN_FOOTER);
-
-        // -------------------------------------------------------------------
-        $pdf->SetFont('dejavusans', '', 14, '', true);
-        // add a page
-        $pdf->AddPage();
-        $col = 30;
-        $line = 10;
-
-        $pdf->Cell( '', 30, $file->file_name, 0, 0, 'C' );
-        $pdf->Ln();
-        $pdf->Cell( $col, $line, 'ФИО', 0, 0, 'L' );
-        $pdf->Cell( '', $line, $record->record_name, 1, 0, 'L' );
-        $pdf->Ln();
-        $pdf->Cell( $col, $line, 'телефон', 0, 0, 'L' );
-        $pdf->Cell( '', $line, $record->record_phone, 1, 0, 'L' );
-        $pdf->Ln();
-        $pdf->Cell( $col, $line, 'Email', 0, 0, 'L' );
-        $pdf->Cell( '', $line, $record->record_email, 1, 0, 'L' );
-        $pdf->Ln();
-        $pdf->Cell( $col, $line, 'Company', 0, 0, 'L' );
-        $pdf->Cell( '', $line, $record->record_company, 1, 0, 'L' );
-        $pdf->Ln();
-        $pdf->Cell( $col, $line, 'City', 0, 0, 'L' );
-        $pdf->Cell( '', $line, $record->record_city, 1, 0, 'L' );
-        $pdf->Ln();
-        $pdf->Cell( $col, $line, 'Region', 0, 0, 'L' );
-        $pdf->Cell( '', $line, $record->record_region, 1, 0, 'L' );
-        $pdf->Ln();
-        $pdf->Cell( $col, $line, 'GUID', 0, 0, 'L' );
-        $pdf->Cell( '', $line, $record->record_guid, 1, 0, 'L' );
-        $pdf->Ln();
-
-        // -------------------------------------------------------------------
-
-        //Close and output PDF document
-        $pdf->Output('example_xxx.pdf', 'I');
+        $pdf = new TCPDF_Outer(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8');
+        $pdf->singleRecordReport($file_id, $id);
         return;
     }
 }
---
+++ app/Utility/TCPDF_Outer.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Utility;
+
+use App\Models\Files;
+use App\Models\Records;
+use TCPDF;
+
+class TCPDF_Outer extends TCPDF
+{
+    public function __construct( $orientation, $unit, $format ) {
+        parent::__construct( $orientation, $unit, $format, true, 'UTF-8', false );
+
+        $this->setCreator(PDF_CREATOR);
+        $this->setAuthor('Oleg Denisenko');
+        $this->setTitle('TCPDF Example 009');
+        $this->setSubject('TCPDF Tutorial');
+        $this->setKeywords('TCPDF, PDF, example, test, guide');
+
+        $header_string= "Ad poenitendum properat, cito qui judicat. \nActum atque tractatum.";
+        $this->setHeaderData('logo.jpg', 20, PDF_HEADER_TITLE, $header_string);
+
+        $this->setHeaderFont(['dejavusans', '', PDF_FONT_SIZE_MAIN]);
+        $this->setFooterFont(['dejavusans', '', PDF_FONT_SIZE_DATA]);
+
+        $this->setDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+        $this->setMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $this->setHeaderMargin(PDF_MARGIN_HEADER);
+        $this->setFooterMargin(PDF_MARGIN_FOOTER);
+    }
+
+    public function singleRecordReport($file_id, $id) {
+        $file = Files::find($file_id);
+        $record = Records::where('file_id', $file_id)
+                         ->where('id', $id)
+                         ->first();
+        $this->sheetСontentGeneration($file, $record);
+        $this->generatePDF('singleRecordReport_' . $id . '.pdf' );
+    }
+    
+    private function sheetСontentGeneration($file, $record) {
+        $col = 30;
+        $line = 10;        
+        $this->SetFont('dejavusans', '', 14, '', true);
+        $this->AddPage();
+        $this->Cell( '', 30, $file->file_name, 0, 0, 'C' );
+        $this->Ln();
+        $this->Cell( $col, $line, 'ФИО', 0, 0, 'L' );
+        $this->Cell( '', $line, $record->record_name, 1, 0, 'L' );
+        $this->Ln();
+        $this->Cell( $col, $line, 'телефон', 0, 0, 'L' );
+        $this->Cell( '', $line, $record->record_phone, 1, 0, 'L' );
+        $this->Ln();
+        $this->Cell( $col, $line, 'Email', 0, 0, 'L' );
+        $this->Cell( '', $line, $record->record_email, 1, 0, 'L' );
+        $this->Ln();
+        $this->Cell( $col, $line, 'Company', 0, 0, 'L' );
+        $this->Cell( '', $line, $record->record_company, 1, 0, 'L' );
+        $this->Ln();
+        $this->Cell( $col, $line, 'City', 0, 0, 'L' );
+        $this->Cell( '', $line, $record->record_city, 1, 0, 'L' );
+        $this->Ln();
+        $this->Cell( $col, $line, 'Region', 0, 0, 'L' );
+        $this->Cell( '', $line, $record->record_region, 1, 0, 'L' );
+        $this->Ln();
+        $this->Cell( $col, $line, 'GUID', 0, 0, 'L' );
+        $this->Cell( '', $line, $record->record_guid, 1, 0, 'L' );
+        $this->Ln();
+    }
+    
+    private function generatePDF(string $file_name) {
+        $this->Output($file_name, 'I');
+        return;
+    }
+}
